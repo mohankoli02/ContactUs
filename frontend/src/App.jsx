3c354cd3d660f2635fe0c89bbe28e4de3ef6a554
mohankoli02 <kolimohanlal02@gmail.com>
2025-02-10 22:28:14 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import ContactForm from "./components/ContactForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
